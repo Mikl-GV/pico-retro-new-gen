c18f4d87cd0c33f7721b519d5e8d639a0510afd2
mikl <mikl@example.com>
2026-09-23 11:21:20 +0300

    debug: FIFO-ожидание перед 'C' в cpu1_entry + баннер билда dcdf7d1

--- h3_bare/src/main.c
+++ h3_bare/src/main.c
@@ -79,7 +79,7 @@ void main(void) {
     uart_init();
     uart_rx_flush();
     uart_puts("\nMultiTool Retro boot\n");
-    uart_puts("build: TFT self-test on CPU1 (477938e)\n");
+    uart_puts("build: TFT self-test dcdf7d1 RXdrain\n");
 
     led_init();
     led_set(0);
